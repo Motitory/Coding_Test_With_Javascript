b17574a38baca31307be590314287ffa3c273bc2
Motitory <motitory@gmail.com>
2023-05-06 06:34:56 +0900

    feat : escape sequence

---
+++ ch01 (Basic Syntax)/escape_sequence.js
@@ -0,0 +1,9 @@
+// Use an escape sequence to output reserved characters or special characters.
+
+// \t: Tab
+// \n: New line
+// \": Double quotation mark
+// \': Single quotation mark
+// \\: Backslash
+
+console.log('"Hello\nWorld"');
