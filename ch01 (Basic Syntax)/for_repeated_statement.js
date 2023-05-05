c7a611e02670e0c2948bdbde10a33f2b2e0e87b3
Motitory <motitory@gmail.com>
2023-05-06 05:46:45 +0900

    feat : for_repeated_statement

---
+++ ch01 (Basic Syntax)/for_repeated_statement.js
@@ -0,0 +1,17 @@
+// If an initialization statement exists, execute it first.
+// If True, execute the code inside the block.
+// If False, skip the block and finish repeated statement.
+// executed the code inside the block, the increment or decrement statement is executed.
+
+```js
+	for (initialization; condition; increment/decrement) {
+		// statements
+	}
+
+	// sum of 1 to 100
+	let summary = 0;
+	for (let i = 1; i <= 100; i++) {
+		summary += i;
+	}
+	console.log(summary);
+```;
