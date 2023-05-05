8aa49d51f36d94ad28801a3d4073f136e42e5dba
Motitory <motitory@gmail.com>
2023-05-06 05:40:31 +0900

    feat : conditional_statement

---
+++ ch01 (Basic Syntax)/conditional_statement.js
@@ -0,0 +1,13 @@
+// condition : a conditional expression that return true or false
+
+``` js
+	if (condition1)
+		statement 1
+	else if (condition2)
+		statement 2
+	else if (condition3)
+		statement 3
+	...
+	else
+		statementN
+```;
